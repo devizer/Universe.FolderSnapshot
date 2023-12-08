d1835cf9cdd97ecebc74934a4e6d1f217bab5853
devizer <devizer@users.github.io>
2023-12-08 19:03:41 +0200

    Test Create snapshot v14

--- Universe.FolderSnapshot/NixSnapshotManager.cs
+++ Universe.FolderSnapshot/NixSnapshotManager.cs
@@ -13,7 +13,7 @@ namespace Universe.FolderSnapshot
 
         public void CreateSnapshot(string sourceFolder, string destinationFile)
         {
-            var pipe = string.IsNullOrEmpty(Compression.FastestCompressPipe) ? "" : Compression.FastestCompressPipe;
+            var pipe = string.IsNullOrEmpty(Compression.FastestCompressPipe) ? "" : $"| {Compression.FastestCompressPipe}";
             string args = $"-e -c \"tar cf - '{sourceFolder}' {pipe} > '{destinationFile}'\"";
             Console.WriteLine($"{Compression.Title} command line: {Environment.NewLine}sh {args}");
             var result = ExecProcessHelper.HiddenExec("sh", args);
