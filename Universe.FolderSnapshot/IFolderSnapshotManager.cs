3931f054c388f993157f5b28481093db1b1e28b1
devizer <devizer@users.github.io>
2023-12-09 05:39:39 +0200

    Fix Universal Test Restore snapshot v3

--- Universe.FolderSnapshot/IFolderSnapshotManager.cs
+++ Universe.FolderSnapshot/IFolderSnapshotManager.cs
@@ -1,58 +1,10 @@
-﻿using System.Collections.Generic;
-
-namespace Universe.FolderSnapshot
+﻿namespace Universe.FolderSnapshot
 {
     public interface IFolderSnapshotManager
     {
         void CreateSnapshot(string sourceFolder, string destinationFile);
         void RestoreSnapshot(string sourceFile, string destinationFolder);
         string Extension { get; }
-
-    }
-
-    public static class FolderSnapshotManagerExtensions
-    {
-        public static List<IFolderSnapshotManager> GetListByPlatform()
-        {
-            var ret = new List<IFolderSnapshotManager>();
-            if (TinyCrossInfo.IsWindows)
-            {
-                ret.Add(new WindowsSnapshotManager());
-            }
-            else
-            {
-                foreach (var compressorDefinition in NixCompressionCatalog.TarCompressors)
-                {
-                    NixSnapshotManager man = new NixSnapshotManager(compressorDefinition);
-                    ret.Add(man);
-                }
-            }
-
-            return ret;
-        }
-
-        public static bool IsSupported(this IFolderSnapshotManager manager)
-        {
-            NixSnapshotManager nixMan = manager as NixSnapshotManager;
-            if (nixMan != null)
-                return nixMan.IsCompressionSupported;
-
-            return true;
-        }
-
-        public static string GetTitle(this IFolderSnapshotManager manager)
-        {
-            NixSnapshotManager nixMan = manager as NixSnapshotManager;
-            if (nixMan != null)
-                return nixMan.Compression.Title;
-
-            if (manager is WindowsSnapshotManager winMan)
-            {
-                return "ZipFile";
-            }
-
-            return manager?.GetType().Name;
-        }
 
     }
 }
