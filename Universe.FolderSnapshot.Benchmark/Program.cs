43e55b66c19421bcc8a67f8672d772117825871b
devizer <devizer@users.github.io>
2023-12-11 20:18:36 +0200

    Experimemntal benchmark ***NO_CI***

--- Universe.FolderSnapshot.Benchmark/Program.cs
+++ Universe.FolderSnapshot.Benchmark/Program.cs
@@ -1,9 +1,12 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿using System.Collections.Immutable;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Parameters;
 using BenchmarkDotNet.Running;
-using Universe.FolderSnapshot.Tests;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Linq;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Loggers;
+
 
 namespace Universe.FolderSnapshot.Benchmark
 {
@@ -11,62 +14,18 @@ namespace Universe.FolderSnapshot.Benchmark
     {
         static void Main(string[] args)
         {
+            var bType = typeof(RestoreSnapshotBenchmarks);
+            var dZip0 = new Descriptor(typeof(RestoreSnapshotBenchmarks), bType.GetMethod("ZipFileUncompressed"));
+            var config = ManualConfig.CreateEmpty().AddJob(Job.Default).AddLogger(ConsoleLogger.Default).AddColumnProvider(EmptyColumnProvider.Instance);
+            ImmutableConfig rc = config.CreateImmutableConfig();
+            ParameterInstances pi = new ParameterInstances(Array.Empty<ParameterInstance>());
+            var zip0 = BenchmarkCase.Create(dZip0, Job.Default, pi, rc);
+            var zip0_b = new BenchmarkRunInfo(new BenchmarkCase[] { zip0 }, bType, rc);
+            
+            // var summary = BenchmarkRunner.Run(zip0_b);
             var summary = BenchmarkRunner.Run<RestoreSnapshotBenchmarks>();
-        }
-    }
+            // BenchmarkRunner
 
-    [RankColumn]
-    [MemoryDiagnoser]
-    public class RestoreSnapshotBenchmarks
-    {
-        private string TestObjectFullPath, TestSnapshotFolder;
-
-        private Lazy<string> _ZipFileUncompressedSnapshot;
-        private Lazy<string> _ZipFileFastestSnapshot;
-        NetZipFileSnapshotManager _ZipFileUncompressedSnapshotManager = new NetZipFileSnapshotManager(NetZipCompressionLevel.NoCompression);
-        NetZipFileSnapshotManager _ZipFileFastestSnapshotManager = new NetZipFileSnapshotManager(NetZipCompressionLevel.Fastest);
-
-        public RestoreSnapshotBenchmarks()
-        {
-            _ZipFileUncompressedSnapshot = new Lazy<string>(GetZipFileUncompressedSnapshot, LazyThreadSafetyMode.ExecutionAndPublication);
-            _ZipFileFastestSnapshot = new Lazy<string>(GetZipFileFastestSnapshot, LazyThreadSafetyMode.ExecutionAndPublication);
-        }
-
-
-        [GlobalSetup]
-        public void GlobalSetup()
-        {
-            TestObjectFullPath = TestEnv.TestObjectFullPath;
-            TestSnapshotFolder = TestEnv.TestSnapshotFolder;
-            var folder1 = _ZipFileUncompressedSnapshot.Value;
-            var folder2 = _ZipFileFastestSnapshot.Value;
-        }
-
-        string GetZipFileUncompressedSnapshot()
-        {
-            var snapshot = Path.Combine(TestEnv.TestSnapshotFolder, $"Snapshot.Uncompressed.{Guid.NewGuid().ToString("N")}.zip");
-            _ZipFileUncompressedSnapshotManager.CreateSnapshot(TestObjectFullPath, snapshot);
-            return snapshot;
-        }
-
-        string GetZipFileFastestSnapshot()
-        {
-            var snapshot = Path.Combine(TestEnv.TestSnapshotFolder, $"Snapshot.Fastest.{Guid.NewGuid().ToString("N")}.zip");
-            _ZipFileFastestSnapshotManager.CreateSnapshot(TestObjectFullPath, snapshot);
-            return snapshot;
-        }
-
-        [Benchmark]
-        public void ZipFileUncompressed()
-        {
-            var restoreTo = Path.Combine(TestEnv.TestSnapshotFolder, $"Restored.ZipFile.Uncompressed.{Guid.NewGuid().ToString("N")}");
-            _ZipFileUncompressedSnapshotManager.RestoreSnapshot(_ZipFileUncompressedSnapshot.Value, restoreTo);
-        }
-        [Benchmark]
-        public void ZipFileFastest()
-        {
-            var restoreTo = Path.Combine(TestEnv.TestSnapshotFolder, $"Restored.ZipFile.Fastest.{Guid.NewGuid().ToString("N")}");
-            _ZipFileFastestSnapshotManager.RestoreSnapshot(_ZipFileFastestSnapshot.Value, restoreTo);
         }
     }
 }
---
+++ Universe.FolderSnapshot.Benchmark/RestoreSnapshotBenchmarks.cs
@@ -0,0 +1,54 @@
+﻿using BenchmarkDotNet.Attributes;
+using Universe.FolderSnapshot.Tests;
+
+namespace Universe.FolderSnapshot.Benchmark;
+
+[RankColumn]
+[MemoryDiagnoser]
+public class RestoreSnapshotBenchmarks
+{
+    private string TestObjectFullPath, TestSnapshotFolder;
+
+    private Lazy<string> _ZipFileUncompressedSnapshot;
+    private Lazy<string> _ZipFileFastestSnapshot;
+    NetZipFileSnapshotManager _ZipFileUncompressedSnapshotManager = new NetZipFileSnapshotManager(NetZipCompressionLevel.NoCompression);
+    NetZipFileSnapshotManager _ZipFileFastestSnapshotManager = new NetZipFileSnapshotManager(NetZipCompressionLevel.Fastest);
+
+    public RestoreSnapshotBenchmarks()
+    {
+        _ZipFileUncompressedSnapshot = new Lazy<string>(GetZipFileUncompressedSnapshot, LazyThreadSafetyMode.ExecutionAndPublication);
+        _ZipFileFastestSnapshot = new Lazy<string>(GetZipFileFastestSnapshot, LazyThreadSafetyMode.ExecutionAndPublication);
+        TestObjectFullPath = TestEnv.TestObjectFullPath;
+        TestSnapshotFolder = TestEnv.TestSnapshotFolder;
+        var folder1 = _ZipFileUncompressedSnapshot.Value;
+        var folder2 = _ZipFileFastestSnapshot.Value;
+    }
+
+    string GetZipFileUncompressedSnapshot()
+    {
+        var snapshot = Path.Combine(TestEnv.TestSnapshotFolder, $"Snapshot.Uncompressed.{Guid.NewGuid().ToString("N")}.zip");
+        _ZipFileUncompressedSnapshotManager.CreateSnapshot(TestObjectFullPath, snapshot);
+        return snapshot;
+    }
+
+    string GetZipFileFastestSnapshot()
+    {
+        var snapshot = Path.Combine(TestEnv.TestSnapshotFolder, $"Snapshot.Fastest.{Guid.NewGuid().ToString("N")}.zip");
+        _ZipFileFastestSnapshotManager.CreateSnapshot(TestObjectFullPath, snapshot);
+        return snapshot;
+    }
+
+    [Benchmark]
+    public void ZipFileUncompressed()
+    {
+        var restoreTo = Path.Combine(TestEnv.TestSnapshotFolder, $"Restored.ZipFile.Uncompressed.{Guid.NewGuid().ToString("N")}");
+        _ZipFileUncompressedSnapshotManager.RestoreSnapshot(_ZipFileUncompressedSnapshot.Value, restoreTo);
+    }
+
+    [Benchmark]
+    public void ZipFileFastest()
+    {
+        var restoreTo = Path.Combine(TestEnv.TestSnapshotFolder, $"Restored.ZipFile.Fastest.{Guid.NewGuid().ToString("N")}");
+        _ZipFileFastestSnapshotManager.RestoreSnapshot(_ZipFileFastestSnapshot.Value, restoreTo);
+    }
+}
